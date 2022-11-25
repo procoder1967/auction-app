2b3ba13575d22d4f046747f4afc72275929923b0
Sulaiman786123 <suk786123@gmail.com>
2022-11-25 17:50:03 +0000

    updated views and urls for sign up

--- auctionapp/urls.py
+++ auctionapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from auctionapp.views import sign_up
 
 urlpatterns = [
     path('',views.base_index,name='base_index'),
+    path('login', sign_up, name= sign_up)
 ]
--- auctionapp/views.py
+++ auctionapp/views.py
@@ -1,7 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def base_index(request):
     return HttpResponse("Hello, world. You're at the base index.")
 
+def sign_up(request):
+    if request.POST == 'POST':
+        form = UserCreationForm()
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully')
+
+    else:
+        form = UserCreationForm()
+    context = {
+        'form':form
+    }
+
+    return render(request, 'register.html', context)
