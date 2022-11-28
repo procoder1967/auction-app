9206dac700759aac715c08d9cd8cc193f4af1876
Sulaiman786123 <suk786123@gmail.com>
2022-11-28 17:00:01 +0000

    Added Login and Signup to views

--- auctionapp/views.py
+++ auctionapp/views.py
@@ -1,32 +1,57 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
 import json
-
 from django.shortcuts import get_object_or_404, render, redirect
 from django.http import Http404
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 
 from .models import User, Profile
-from .forms import LoginForm, SignupForm
+from .forms import Login, SignupForm
 
 # Create your views here.
 def base_index(request):
     return HttpResponse("Hello, world. You're at the base index.")
 
 def sign_up(request):
-    if request.POST == 'POST':
-        form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        username = form.cleaned_data['username']
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['password']
+        newUser = User.objects.create(email = email, username =username)
+        newUser.set_password(password)
+        newUser.save()
+
+        user =auth.authenticate(username=username, email=email, password =password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('') #FILL IN
+    
+    return render(request, 'mainapp/auth/signup.html', {'form': SignupForm})
+
+def login_view(request):
+
+    if request.method == 'POST':
+        form = Login(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Account created successfully')
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            
+            user =auth.authenticate(username=username, email=email, password =password)
+            if user is not None:
+                auth.login(request,user)
+                return redirect('') #FILL IN
+            
+            return render(request, 'error.html', {
+                'error': 'User not registered. Sign up first.'
+            })
 
-    else:
-        form = UserCreationForm()
-    context = {
-        'form':form
-    }
+        # invalid form
+        return render(request, 'mainapp/auth/login.html', {
+            'form': form
+        })
 
-    return render(request, 'register.html', context)
-
+    return render(request, 'mainapp/auth/login.html', {'form': form})
