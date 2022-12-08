2d2c8d56993aed65582f7ecafb97d2a41e467977
iliasselyaak <iliasselyaa@hotmail.com>
2022-12-08 19:48:48 +0000

    modal component working

--- auctionapp/frontend/src/components/AddTask.vue
+++ auctionapp/frontend/src/components/AddTask.vue
@@ -1,18 +1,31 @@
 <template>
 <div>
-    
+  <a class="py-2 px-3 hover:text-red-500 cursor-pointer" @click="showModal = true">+ Add Task</a>
+
+  <BaseModal :show="showModal">
+    <div class="p-4">
+      <div class="text-lg">Hello Modal World!</div>
+
+      <div class="py-2 text-sm">Click to close:</div>
+
+      <button type="button" class="bg-indigo-200 px-3 py-1 font-medium" @click="showModal = false">
+        Hide modal
+      </button>
+    </div>
+  </BaseModal>
 </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref} from 'vue'
+import BaseModal from './modals/BaseModal.vue'
 
 export default defineComponent({
-    setup () {
-        
-
-        return {}
-    }
+    components: {BaseModal},
+    setup(){
+        const showModal = ref(false);
+        return{showModal}
+    },
 })
 </script>
 
---
+++ auctionapp/frontend/src/components/modals/BaseModal.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- Render inside our `<div id="modals"></div>` in index.html -->
+  <Teleport to="#modals">
+    <!-- Show / hide the modal -->
+    <div v-if="show" class="">
+      <!-- The backdrop -->
+      <div class="fixed inset-0 bg-gray-900 opacity-40"></div>
+
+      <!-- Where the actual content goes -->
+      <div class="fixed inset-0 flex items-center justify-center">
+        <div class="bg-white text-black">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </Teleport>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        show: Boolean
+    },
+    setup(props) {
+        props.show
+    }
+})
+</script>
