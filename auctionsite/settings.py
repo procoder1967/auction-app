02fd6fad05be79d15b675d9871e4d352c95b8d53
Shorya Sinha <shoryaoct20@gmail.com>
2022-12-08 18:45:38 +0000

    forms done, css work to be done, created superuser

--- auctionsite/settings.py
+++ auctionsite/settings.py
@@ -33,7 +33,7 @@ AUTH_USER_MODEL = "auctionapp.User"
 # Application definition
 
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
